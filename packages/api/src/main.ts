ce8941078e43923c6da0422530ea72aa54d274b8
Lee-KyungSeok <leeseok8347@gmail.com>
2023-02-26 01:02:57 +0900

    Add sdk

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,9 +1,23 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as swaggerJson from './swagger.json';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const copied = {
+        ...swaggerJson,
+        servers: [],
+        info: {
+            title: "Sharing dto example swagger",
+            description: "Sharing dto example swagger",
+            version: "1.1",
+            contact: {}
+        },
+    }
+    SwaggerModule.setup('docs', app, copied as any)
 
     const port = 3000;
 
